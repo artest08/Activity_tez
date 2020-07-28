4ec6624cc811b34852963878c3efdd7c39204599
Muhammet Esat Kalfaoglu <esatkalfaoglu@gmail.com>
2020-07-28 19:02:03 +0300

    argumen for 2DCNN temporal pooling is corrected

--- two_stream_bert.py
+++ two_stream_bert.py
@@ -33,7 +33,7 @@ import swats
 
 
 model_names = sorted(name for name in models.__dict__
-    if name.islower() and not name.startswith("__")
+    if not name.startswith("__")
     and callable(models.__dict__[name]))
 
 dataset_names = sorted(name for name in datasets.__all__)
