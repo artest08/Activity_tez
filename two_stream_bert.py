206caf7885f973bec26e0b522f4b3d9e47d591e6
Muhammet Esat Kalfaoglu <esatkalfaoglu@gmail.com>
2020-07-30 15:57:11 +0300

    pooling5 is added for 2D CNN

--- two_stream_bert.py
+++ two_stream_bert.py
@@ -14,7 +14,7 @@ import numpy as np
 
 
 # os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
-# os.environ["CUDA_VISIBLE_DEVICES"]="1"
+# os.environ["CUDA_VISIBLE_DEVICES"]="0"
 
 import torch
 import torch.nn as nn
@@ -49,7 +49,7 @@ parser.add_argument('--settings', metavar='DIR', default='./datasets/settings',
 parser.add_argument('--dataset', '-d', default='hmdb51',
                     choices=["ucf101", "hmdb51", "smtV2", "window"],
                     help='dataset: ucf101 | hmdb51')
-parser.add_argument('--arch', '-a', metavar='ARCH', default='rgb_resnet18_convGRUType3',
+parser.add_argument('--arch', '-a', metavar='ARCH', default='rgb_resnet18_pooling5',
                     choices=model_names,
                     help='model architecture: ' +
                         ' | '.join(model_names) +
